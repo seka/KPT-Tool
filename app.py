2b2d5c380a832e60a62ac72ee1e711de22bb7388
sekaryo <asutamuse@gmail.com>
2015-01-21 20:25:07 +0900

    サインイン・サインアウト・サインアップ用のルーティングの作成

--- app.py
+++ app.py
@@ -11,5 +11,17 @@ app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 def index():
     return render_template("login.html")
 
+@app.route('/signin', methods=["POST"])
+def signin():
+    pass
+
+@app.route('/signout', methods=["POST"])
+def signout():
+    pass
+
+@app.route('/signup', methods=["POST"])
+def signout():
+    pass
+
 if __name__ == "__main__":
     app.run()
