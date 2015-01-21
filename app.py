38fda8f1433cdbef4ea3b4e93b85c65c529f8de5
sekaryo <asutamuse@gmail.com>
2015-01-22 03:07:21 +0900

    テストユーザオブジェクトと一致するユーザ名とパスワード（素の状態）を入力するとログインする機能を実装

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+test_user = {
+  "test" : {
+    "name" : "test"
+    , "password" : "29148931"
+  }
+}
 
 @app.route("/")
 def index():
@@ -13,14 +20,25 @@ def index():
 
 @app.route('/signin', methods=["POST"])
 def signin():
-    pass
+  req = {
+    "username" : request.form["username"]
+    , "password" : request.form["password"]
+  }
+
+  user = test_user
+  result = user.has_key(req["username"])
+
+  if result == False or user[req["username"]]["password"] != req["password"]:
+    return render_template("login.html", error="IDまたはパスワードが違います")
+  return render_template("test.html")
+
 
 @app.route('/signout', methods=["POST"])
 def signout():
     pass
 
 @app.route('/signup', methods=["POST"])
-def signout():
+def signup():
     pass
 
 if __name__ == "__main__":
